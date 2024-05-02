a5fb3f492abd0262a3783cf24078037db3d043f8
Stephane van Gulick <stephane.vangulick@swisscom.com>
2024-05-02 08:15:41 +0200

    First official release

--- PsKrane/PsKrane.psm1
+++ PsKrane/PsKrane.psm1
@@ -1,21 +1,3 @@
-<#
-.SYNOPSIS
-    Build script for the module
-.DESCRIPTION
-    A longer description of the function, its purpose, common use cases, etc.
-.NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
-.LINK
-    Specify a URI to a help page, this will show when Get-Help -Online is used.
-.EXAMPLE
-    Test-MyTestFunction -Verbose
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
-#>
-
-
-
-
-#Create
 Class ModuleBuild {
     [String]$ModuleName
     [System.IO.FileInfo]$ModuleFile
@@ -26,19 +8,21 @@ Class ModuleBuild {
     [System.IO.DirectoryInfo]$Tests
     [System.IO.DirectoryInfo]$Outputs
     [System.IO.DirectoryInfo]$DestinationFolder
-    [String[]] $Tags= @( 'PSEdition_Core', 'PSEdition_Desktop' )
+    [String[]] $Tags = @( 'PSEdition_Core', 'PSEdition_Desktop' )
+    [String]$Description
+    [String]$ProjectUri
 
     #Add option Overwrite
 
-    ModuleBuild([System.IO.DirectoryInfo]$Root,[String]$ModuleName) {
+    ModuleBuild([System.IO.DirectoryInfo]$Root, [String]$ModuleName) {
         $this.Root = $Root
         $this.Build = "$Root\Build"
         $this.Sources = "$Root\Sources"
         $this.Tests = "$Root\Tests"
         $this.Outputs = "$Root\Outputs"
         $this.ModuleName = $ModuleName
-        if(($this.Build.Exists -eq $false) -or ($this.Sources.Exists -eq $false) -or ($this.Tests.Exists -eq $false)) {
-           Throw "No Build, Sources or Tests folder found in $($This.Root)"
+        if (($this.Build.Exists -eq $false) -or ($this.Sources.Exists -eq $false) -or ($this.Tests.Exists -eq $false)) {
+            Throw "No Build, Sources or Tests folder found in $($This.Root)"
         }
 
         $this.FetchModuleInfo()
@@ -46,7 +30,7 @@ Class ModuleBuild {
 
     hidden FetchModuleInfo() {
 
-        if(($null -eq $this.ModuleName)){
+        if (($null -eq $this.ModuleName)) {
             Throw "Module Name not provided."
         }
         
@@ -55,15 +39,15 @@ Class ModuleBuild {
         
     }
 
-    BuildModule(){
+    BuildModule() {
         
-        if ($this.ModuleFile.Exists){
+        if ($this.ModuleFile.Exists) {
             $this.ModuleFile.Delete()
             $this.ModuleFile.Refresh()
         }
 
         Write-Verbose "[BUILD][START]  Creating module file."
-        $Null = New-Item -Path $this.ModuleFile.FullName -ItemType "file"
+        $Null = New-Item -Path $this.ModuleFile.FullName -ItemType "file" -Force
         
 
         $MainPSM1Contents = @()
@@ -127,10 +111,10 @@ Class ModuleBuild {
         
         Write-verbose "[BUILD][START][PSD1] Starding PSD1 actions. Adding functions to export"
 
-        if (!$this.ModuleDataFile.Exists){
+        if (!$this.ModuleDataFile.Exists) {
             New-ModuleManifest -Path $this.ModuleDataFile.FullName
         }
-        Update-ModuleManifest -Path $this.ModuleDataFile.FullName -FunctionsToExport $Publicfunctions.BaseName -Tags $This.Tags -RootModule $this.ModuleFile.Name
+        Update-ModuleManifest -Path $this.ModuleDataFile.FullName -FunctionsToExport $Publicfunctions.BaseName -Tags $This.Tags -RootModule $this.ModuleFile.Name -Description $this.Description -ProjectUri $this.ProjectUri
 
         
 
@@ -153,7 +137,7 @@ Class ModuleObfuscator {
     [System.IO.FileInfo]$BinaryModuleFile
     [System.IO.FileInfo]$ModuleDataFile
 
-    Obfuscator(){}
+    Obfuscator() {}
 
     SetModuleBuild([ModuleBuild]$Module) {
         $Module.ModuleDataFile.Refresh()
@@ -168,7 +152,7 @@ Class ModuleObfuscator {
         
     }
 
-    Obfuscate(){
+    Obfuscate() {
 
         Write-Verbose "[BUILD][OBFUSCATE] Obfuscating module"
         Write-Verbose "[BUILD][OBFUSCATE] Starting psd1 operations"
@@ -190,21 +174,21 @@ Class ModuleObfuscator {
     }
 }
 
-Function New-ModuleBuild{
+Function New-KraModuleBuild {
     [cmdletBinding()]
     Param(
-        [Parameter(Mandatory=$False,HelpMessage="Root folder of the project. If not specified, it assumes it is located in a folder called 'Build' in the root of the project.")]
+        [Parameter(Mandatory = $False, HelpMessage = "Root folder of the project. If not specified, it assumes it is located in a folder called 'Build' in the root of the project.")]
         [System.IO.DirectoryInfo]$Root,
-        [Parameter(Mandatory=$true,HelpMessage="Name of the module to build.")]
+        [Parameter(Mandatory = $true, HelpMessage = "Name of the module to build.")]
         [String]$ModuleName
     )
 
-    if(($Root.Exists -eq $false)) {
+    if (($Root.Exists -eq $false)) {
         Throw "Root $($Root.FullName) folder not found"
     }
 
     # Retrieve parent folder
-    if(!$Root){
+    if (!$Root) {
         
         $Current = (Split-Path -Path $MyInvocation.MyCommand.Path)
         $Root = ((Get-Item $Current).Parent).FullName
@@ -213,22 +197,84 @@ Function New-ModuleBuild{
 
     #Creating the ModuleBuild (simple PSM1 + PSD1)
     $VerbosePreference = 'Continue'
-    $ModuleBuild = [ModuleBuild]::New($Root,$ModuleName)
+    $ModuleBuild = [ModuleBuild]::New($Root, $ModuleName)
     Return $ModuleBuild
 }
 
-<#
+Class NuSpecFile {
+    [ModuleBuild]$ModuleBuild
+    [String]$Version
+    [System.IO.DirectoryInfo]$ExportFolderPath
+    [System.IO.FileInfo]$NuSpecFilePath
+    hidden [String]$RawContent
 
-#Creating the Obfuscated version of the module based on the ModuleBuild
-$Obfuscator= [ModuleObfuscator]::NeW()
-$Obfuscator.SetModuleBuild($ModuleBuild)
-$Obfuscator.Obfuscate()
-#>
+    NuspecFile([ModuleBuild]$ModuleBuild) {
+        $this.SetModuleBuild($ModuleBuild)
+        $this.ExportFolderPath = Join-Path -Path $this.ModuleBuild.Outputs -ChildPath "Nuget"
+    }
 
+    SetModuleBuild([ModuleBuild]$ModuleBuild) {
+        $this.ModuleBuild = $ModuleBuild
+    }
 
+    hidden [Void]Generate() {
+        $psd1Data = Import-PowerShellDataFile -Path $this.ModuleBuild.ModuleDataFile
+        $NuSpecString = @"
+<?xml version="1.0" encoding="utf-8"?>
+<package>
+    <metadata>
+    <id>{0}</id>
+    <version>{1}</version>
+    <authors>{2}</authors>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <license type="expression">MIT</license>
+    <!-- <icon>icon.png</icon> -->
+    <projectUrl>{3}</projectUrl>
+    <description>{4}</description>
+    <releaseNotes>{5}</releaseNotes>
+    <copyright>Copyright All rights reserved</copyright>
+    <tags>{5}</tags>
+    <dependencies>
+    </dependencies>
+    </metadata>
+</package>
+"@
 
+        
+        $Id = $this.ModuleBuild.ModuleName #0
+        $this.Version = $psd1Data.ModuleVersion #1
+        $Authors = $psd1Data.Author #2
+        $ProjectUri = $psd1Data.PrivateData.PsData.ProjectUri #3
+        $Description = $psd1Data.Description #4
+        $ReleaseNotes = $psd1Data.releaseNotes #4
+        $Tags = $psd1Data.PrivateData.PsData.tags -join "," #5
+        $Final = $NuSpecString -f $Id, $this.Version, $Authors, $ProjectUri, $Description, $Tags
+        $this.RawContent = $Final
+    }
 
-#Release Pipeline
-#Todo Change name of the Published artifact
-#Todo Change release name to bin
-#Todo 
+    [System.IO.FileInfo] CreateNuSpecFile() {
+        $this.Generate()
+
+        $Modulefolder = Join-Path -Path $this.ModuleBuild.Outputs.FullName -ChildPath "Module"
+        $this.NuSpecFilePath = Join-Path -Path $Modulefolder -ChildPath ($this.ModuleBuild.ModuleName + ".nuspec")
+        $this.RawContent | Out-File -FilePath $this.NuspecFilePath -Encoding utf8 -Force
+        Return $this.NuspecFilePath
+
+    }
+
+    CreateNugetFile() {
+        & nuget pack $this.NuSpecFilePath.FullName -OutputDirectory $this.ExportFolderPath -Version "1.0.0"
+    }
+}
+
+Function New-KraNuspecFile {
+    Param(
+        [Parameter(Mandatory = $True)]
+        [ModuleBuild]$ModuleBuild
+    )
+
+    $NuSpec = [NuSpecFile]::New($ModuleBuild)
+    $NuSpec.CreateNuSpecFile()
+    $NuSpec.CreateNugetFile()
+}
+
