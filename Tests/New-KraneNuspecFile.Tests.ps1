1171ab60520fed95db3fad8f5d81c24201cdb514
Laurent Lienhard <llienhard@cw.fmlogistic.com>
2024-11-12 22:14:35 +0100

    Correct pester test New-KraneNuspecFile with test for Nuget Package

--- Tests/New-KraneNuspecFile.Tests.ps1
+++ Tests/New-KraneNuspecFile.Tests.ps1
@@ -10,10 +10,14 @@ Describe "New-KraneNuspecFile" {
             $NewProject = New-KraneProject -Type $ProjectType -Name $ProjectName -Path $ProjectPath
             New-KraneNuspecFile -KraneModule $NewProject
             $NuspecFile = Join-Path -Path $NewProject.Outputs.FullName -ChildPath "Module\$($NewProject.ModuleName).nuspec"
+            $NugetPackage = Join-Path -Path $NewProject.Outputs.FullName -ChildPath "Nuget\$($NewProject.ModuleName).$($NewProject.ProjectVersion).nuspec"
         }
 
         It "Nuspec file should be created" {
             $NuspecFile.Exists | Should -BeTrue
+        }
+        It "Nuspec package should be ceated" {
+            $NugetPackage.Exists | Should -BeTrue
         }
     }
 }
