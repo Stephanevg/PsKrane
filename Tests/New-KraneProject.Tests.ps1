f6afe668ffc557c46066fd7bac85fc952ec6881c
Laurent Lienhard <llienhard@cw.fmlogistic.com>
2024-11-12 16:12:29 +0100

    add pester test new-KraneProject

--- Tests/New-KraneProject.Tests.ps1
+++ Tests/New-KraneProject.Tests.ps1
@@ -1,6 +1,30 @@
-Describe "Testing Write-Host" {
-    It "Should output 'plop'" {
-        $output = Write-output "plop"
-        $output | Should -Be "plop"
+Describe "New-KraneProject" {
+    Context "new project of type module with valid parameters" {
+        BeforeAll {
+            # Arrange
+            $type = 'Module'
+            $name = 'TestProject'
+            $path = $TestDrive
+       
+            # Act
+            $result = New-KraneProject -Type $type -Name $name -Path $path
+        }
+
+        It "KraneProject should not be null " {
+            # Assert
+            $result | Should -Not -BeNullOrEmpty
+        }
+        It "KraneProject module name should correct" {
+            $result.ModuleName | Should -Be $name
+        }
+        It "KraneProject root path should be correct" {
+            $result.Root | Should -Be "$($path)\$($name)"
+        }
+        It "KraneProject type should be correct" {
+            $result.ProjectType | Should -Be $type
+        }
+        It "KraneProject file should exist" {
+            (Test-Path $result.KraneFile.Path) | Should -Be $True
+        }
     }
 }
