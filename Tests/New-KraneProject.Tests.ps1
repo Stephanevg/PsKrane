9cc7c7d947bb563e3a0241a036294526b2f5e95f
Laurent Lienhard <llienhard@cw.fmlogistic.com>
2024-12-01 15:53:00 +0100

    Update New-KraneProject test to use inmodule scope option

--- Tests/New-KraneProject.Tests.ps1
+++ Tests/New-KraneProject.Tests.ps1
@@ -1,48 +1,56 @@
-Describe "New-KraneProject" {
-    Context "new project of type module with valid parameters" {
-        BeforeAll {
-            # Arrange
-            $ProjectType = 'Module'
-            $ProjectName = 'TestProject'
-            $ProjectPath = $TestDrive
+# Generated with love using PsKrane
 
-            
-            # Act
-            $NewProject = New-KraneProject -Type $ProjectType -Name $ProjectName -Path $ProjectPath
-        }
+[System.IO.DirectoryInfo]$psroot = $PSScriptRoot
 
-        It "KraneProject should not be null " {
-            $NewProject | Should -Not -BeNullOrEmpty
-        }
-        It "KraneProject module name should correct" {
-            $NewProject.ModuleName | Should -Be $ProjectName
-        }
-        It "KraneProject root path should be correct" {
-            $NewProject.Root | Should -Be "$($ProjectPath)\$($ProjectName)"
-        }
-        It "KraneProject type should be correct" {
-            $NewProject.ProjectType | Should -Be $ProjectType
-        }
-        It "KraneProject file should exist" {
-            (Test-Path $NewProject.KraneFile.Path) | Should -Be $True
-        }
-        It "KraneProject file path should be correct" {
-            $NewProject.KraneFile.Path.FullName | Should -Be "$($ProjectPath)\$($ProjectName)\.krane.json"
-        }
+Get-Module -Name $psroot.Parent.Name | Remove-Module -Force
+Import-Module  (Join-Path -Path $psroot.Parent -ChildPath "$($psroot.Parent.Name)/$($psroot.Parent.Name).psm1") -Force
 
-        It "KraneProject description should be correct" {
-            $NewProject.Description | Should -Be $null
-        }
+InModuleScope -ModuleName $psroot.Parent.Name -ScriptBlock {
+    Describe "New-KraneProject" {
+        Context "new project of type module with valid parameters" {
+            BeforeAll {
+                # Arrange
+                $ProjectType = 'Module'
+                $ProjectName = 'TestProject'
+                $ProjectPath = $TestDrive
+                
+                # Act
+                $NewProject = New-KraneProject -Type $ProjectType -Name $ProjectName -Path $ProjectPath
+            }
 
-        It "KraneProject project uri should be correct" {
-            $NewProject.ProjectUri | Should -Be $null
+            It "KraneProject should not be null " {
+                $NewProject | Should -Not -BeNullOrEmpty
+            }
+            It "KraneProject module name should correct" {
+                $NewProject.ModuleName | Should -Be $ProjectName
+            }
+            It "KraneProject root path should be correct" {
+                $NewProject.Root | Should -Be "$($ProjectPath)\$($ProjectName)"
+            }
+            It "KraneProject type should be correct" {
+                $NewProject.ProjectType | Should -Be $ProjectType
+            }
+            It "KraneProject file should exist" {
+                (Test-Path $NewProject.KraneFile.Path) | Should -Be $True
+            }
+            It "KraneProject file path should be correct" {
+                $NewProject.KraneFile.Path.FullName | Should -Be "$($ProjectPath)\$($ProjectName)\.krane.json"
+            }
+    
+            It "KraneProject description should be correct" {
+                $NewProject.Description | Should -Be $null
+            }
+    
+            It "KraneProject project uri should be correct" {
+                $NewProject.ProjectUri | Should -Be $null
+            }
+            It "KraneProject templates should not be empty" {
+                $NewProject.Templates.Templates | Should -Not -BeNullOrEmpty
+            }
         }
-        It "KraneProject templates should not be empty" {
-            $NewProject.Templates.Templates | Should -Not -BeNullOrEmpty
+    
+        Context "new project of type script with valid parameters" {
+            #Not implemented yet
         }
-    }
-
-    Context "new project of type script with valid parameters" {
-        #Not implemented yet
     }
 }
